cbf32e7f3d0064bff454c5a466eff16eb780651f
Abbas <y.abbas.habib@gmail.com>
2022-05-30 08:35:02 +0200

    Added a udp reciever at unity not working prop

---
+++ UDPReceive.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPReceive : MonoBehaviour
+{
+    Thread receiveThread;
+
+    UdpClient client;
+
+    [SerializeField]
+    private int port; 
+
+    private string lastReceivedUDPPacket = "";
+
+    public void Start()
+    {
+
+        init();
+    }
+
+    // OnGUI
+    void OnGUI()
+    {
+        Rect rectObj = new Rect(40, 10, 200, 400);
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        GUI.Box(rectObj, "# UDPReceive\n127.0.0.1 " + port + " #\n"
+                    + "\nLast Packet: \n" + lastReceivedUDPPacket
+                , style);
+    }
+
+    // init
+    private void init()
+    {
+        print("UDPSend.init()");
+
+        // status
+        print("Sending to 127.0.0.1 : " + port);
+        print("Test-Sending to this Port: nc -u 127.0.0.1  " + port + "");
+
+        receiveThread = new Thread(
+            new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+    }
+
+    // receive thread
+    private void ReceiveData()
+    {
+
+        client = new UdpClient(port);
+        while (true)
+        {
+
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+
+ 
+                string text = Encoding.UTF8.GetString(data);
+
+                print(">> " + text);
+
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    public string getLatestUDPPacket()
+    {
+        return lastReceivedUDPPacket;
+    }
+}
