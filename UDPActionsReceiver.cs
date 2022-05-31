b9054cc87e09a9b9d75644c48cf410b77a776765
Abbas <y.abbas.habib@gmail.com>
2022-05-31 05:06:47 +0200

    action receiver on seperate thread

---
+++ UDPActionsReceiver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class UDPActionsReceiver : MonoBehaviour
+{
+
+    // Update is called once per frame
+    private String currAction = "";
+    public static UDPActionsReceiver Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    void Update()
+    {
+        while (UDPReceive.actionQueue.TryDequeue(out currAction))
+            print(">>>>> " + currAction);
+    }
+
+
+    public String getGyroscopeVals()
+    {
+        return "";
+    }
+
+}
