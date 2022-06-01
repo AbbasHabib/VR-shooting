5cf0b0b5d7834a60b52cbb3e5b65d5b82e180072
Abbas <y.abbas.habib@gmail.com>
2022-06-02 00:28:51 +0200

    change in esp UDP packets + broadcasting

--- espUDP.ino
+++ espUDP.ino
@@ -3,27 +3,32 @@
 
 #define WIFI_SSID "net1"
 #define WIFI_PASS "yoyoyoyo"
-#define UDP_PORT 4210
+#define RECEIVER_IP "255.255.255.255"
+
+#define LISTENING_UDP_PORT  8051
+#define SENDING_TO_PORT     8051
 
 WiFiUDP UDP;
 
-IPAddress local_IP(192, 168, 137, 46);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(8, 8, 8, 8);   //optional
-IPAddress secondaryDNS(8, 8, 4, 4); //optional
+char buff [81];
+
+//IPAddress local_IP(192, 168, 137, 46);
+//IPAddress gateway(192, 168, 1, 1);
+//IPAddress subnet(255, 255, 255, 0);
+//IPAddress primaryDNS(8, 8, 8, 8);   //optional
+//IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
 
-IPAddress REMOTE_IP = NULL;
-uint16_t  REMOTE_PORT = NULL;
+//IPAddress REMOTE_IP = NULL;
+//uint16_t  REMOTE_PORT = NULL;
 
 char packet_buffer[255];
   
 void setup() {
-  Serial.begin(115200);
-  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-    Serial.println("STA Failed to configure");
-  }
+  Serial.begin(38400);
+//  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+//    Serial.println("STA Failed to configure");
+//  }
   Serial.println();
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   
@@ -43,36 +48,37 @@ void setup() {
   Serial.println(WiFi.localIP());
  
   // Begin listening to UDP port
-  UDP.begin(UDP_PORT);
+  UDP.begin(LISTENING_UDP_PORT);
   Serial.print("Listening on UDP port ");
-  Serial.println(UDP_PORT);
-  wait_for_connection_msg();
+  Serial.println(LISTENING_UDP_PORT);
+  buff[80] = '\0';
 }
 
-void wait_for_connection_msg(){
-  int packetSize = UDP.parsePacket();
-  if (packetSize) {
-    Serial.print("Received packet! Size: ");
-    Serial.println(packetSize); 
-    int len = UDP.read(packet_buffer, 255);
-    if (len > 0)
-      packet_buffer[len] = '\0';
-    Serial.print("Packet received: ");
-    Serial.println(packet_buffer);
-    REMOTE_IP = UDP.remoteIP();
-    REMOTE_PORT = UDP.remotePort();
-  }
-}
-
-void send_packet(char* msg){
-  UDP.beginPacket(REMOTE_IP, REMOTE_PORT);
+void sendPacket(char* msg){
+  UDP.beginPacket(RECEIVER_IP, SENDING_TO_PORT);
   UDP.write(msg);
   UDP.endPacket();
 }
 
 void loop() {
-    if(!REMOTE_IP || !REMOTE_PORT)
-      wait_for_connection_msg();
-    send_packet("sensors data 544, 5456, 84488 ,64 ,548 , 20");
-    delay(10);
+  static int indx = 0;
+  static char c= '\0';
+  while (Serial.available() > 0) {
+    c = Serial.read();
+    if(c== '\n' || c == '\r'){
+      indx = 0;
+      break;
+    }
+    if (indx < 80) {
+      buff[indx++] = c; // save data in the next index in the array buff
+      if (buff[indx-1] == '#') { //check for the end of the word
+        buff[indx-1] = '\0';
+        sendPacket(buff);
+        indx = 0;
+      }
+    }
+    else
+      indx = 0;
+  }
+  
 }
